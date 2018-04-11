78bc253747a8828cee0ac40af827877f9ec3586c
microcosm1994 <microcosm1994@gmail.com>
2018-04-11 21:32:51 +0800

    添加了豆瓣电影

--- app-server/app.py
+++ app-server/app.py
@@ -1,28 +1,55 @@
 from flask import Flask, request
+from bs4 import BeautifulSoup
 import requests
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    url = 'https://cospace.cc/api/works/ranking?p=1&size=20&type=fixed_day&original=0'
+@app.route('/video/<type>', methods=['GET', 'POST'])
+def video(type):
+    print(type)
+    url = 'https://movie.douban.com/'
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:53.0) Gecko/20100101 Firefox/53.0',
         'Accept - Language': 'zh - CN, zh;q = 0.8, en - US;q = 0.5, en;q = 0.3'
     }
     r = requests.get(url, headers=headers)
-    data = r.json()
-    result = ''
-    for item in data:
-        if item == 'data':
-            d = data[item]
-            result = d['works']
-            for value in result:
-                print(value)
-                print(type(value))
-    return json.dumps(result)
+    r.encoding = 'utf-8'
+    #
+    soup = BeautifulSoup(r.text, from_encoding='utf-8')
+    if type == 'screening':
+        screening = soup.select('.screening-bd .ui-slide-content .ui-slide-item')
+        screening_data = []
+        for item in screening:
+            obj = {}
+            if len(item.select('ul')) != 0:
+                obj['cover'] = item.select('.poster a img')[0]['src']
+                obj['url'] = item.select('.poster a')[0]['href']
+                obj['title'] = item.select('.title a')[0].string
+                if len(item.select('.rating .subject-rate')) != 0:
+                    obj['rating'] = item.select('.rating .subject-rate')[0].string
+                else:
+                    obj['rating'] = '0.0'
+                obj['ticket'] = item.select('.ticket_btn span a')[0]['href']
+                screening_data.append(obj)
+        return json.dumps(screening_data)
+    if type == 'hotmovie':
+        hotmovie = soup.select('.screening-bd .ui-slide-content .ui-slide-item')
+        hotmovie_data = []
+        for item in hotmovie:
+            obj = {}
+            if len(item.select('ul')) != 0:
+                obj['cover'] = item.select('.poster a img')[0]['src']
+                obj['url'] = item.select('.poster a')[0]['href']
+                obj['title'] = item.select('.title a')[0].string
+                if len(item.select('.rating .subject-rate')) != 0:
+                    obj['rating'] = item.select('.rating .subject-rate')[0].string
+                else:
+                    obj['rating'] = '0.0'
+                obj['ticket'] = item.select('.ticket_btn span a')[0]['href']
+                hotmovie_data.append(obj)
+        return json.dumps(hotmovie_data)
 
 
 @app.route('/find/<username>')
---
+++ app_web/pages/video/video.js
@@ -0,0 +1,77 @@
+// pages/video/video.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title:'豆瓣热门电影',
+    video_list: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var self = this
+    wx.request({
+      url: 'http://localhost:5000/video/screening',
+      method: 'GET',
+      success: function (response) {
+        console.log(response)
+        self.setData({
+          video_list: response.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
